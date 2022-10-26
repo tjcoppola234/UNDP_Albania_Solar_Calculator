2ea1984573b94e746d15e045a86f153ea03d4cd1
tjcoppola234 <37402171+tjcoppola234@users.noreply.github.com>
2022-10-26 17:33:39 +0200

    new logos, better spacing

--- src/App.js
+++ src/App.js
@@ -15,12 +15,15 @@ function App() {
 export function NavBar() {
   return (
       <nav className="Nav-bar">
+        <div className="Nav-bar-logos">
           <img id="undp-logo" src={process.env.PUBLIC_URL+'UNDP_logo.png'} alt='Logo for UNDP'></img>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/Testpath">testpath</Link></li>
-            <li><Link to="/FAQ">FAQ</Link></li>
-          </ul>
+          <img id="wpi-logo" src={process.env.PUBLIC_URL+'WPI_logo.png'} alt='Logo for WPI'></img>
+        </div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/Testpath">testpath</Link></li>
+          <li><Link to="/FAQ">FAQ</Link></li>
+        </ul>
       </nav>
   );
 }
