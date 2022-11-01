ba0ad162f15a9240aafadf913f7fbecd1e8e3ffc
Andrew Salls <asallsschool@gmail.com>
2022-11-01 12:05:47 +0100

    Updated design of nav bar
    Changed colors and nav bar design to feel more modern

--- src/App.js
+++ src/App.js
@@ -8,29 +8,31 @@ function App() {
     <div className="App">
       <PageHead></PageHead>
       <h2>Homepage</h2>
+      <div className="Nav-bar-logos">
+        <img id="undp-logo" src={process.env.PUBLIC_URL+'UNDP_logo.png'} alt='Logo for UNDP'></img>
+        <img id="wpi-logo" src={process.env.PUBLIC_URL+'WPI_logo.png'} alt='Logo for WPI'></img>
+      </div>
     </div>
   );
 }
 
 export function PageHead() {
   return (
-    <header className="App-header">
-      <h1>
-        Shqipëri Solar Calculator
-      </h1>
-      <nav className="Nav-bar">
-        <div className="Nav-bar-logos">
-          <img id="undp-logo" src={process.env.PUBLIC_URL+'UNDP_logo.png'} alt='Logo for UNDP'></img>
-          <img id="wpi-logo" src={process.env.PUBLIC_URL+'WPI_logo.png'} alt='Logo for WPI'></img>
-        </div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Resources">Resources</Link></li>
-          <li><Link to="/Calculator">Calculator</Link></li>
-          <li><Link to="/FAQ">FAQ</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <div className="Nav-spacing">
+      <header className="App-header">
+        <h1>
+          Shqipëri Solar Calculator
+        </h1>
+        <nav className="Nav-bar">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/Resources">Resources</Link></li>
+            <li><Link to="/Calculator">Calculator</Link></li>
+            <li><Link to="/FAQ">FAQ</Link></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
 
