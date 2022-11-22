b80c3cc9f2a78ca4bceb4fd26d4c4fba7c396bcd
annalisallan <annalisaallan03@gmail.com>
2022-11-22 12:50:42 +0100

    resource changes

--- src/Resources.js
+++ src/Resources.js
@@ -15,59 +15,59 @@ function Resources() {
                      
             <h2 id="h2resources">Resources </h2>
 
-            <details close> {/* place "open" next to "details" to make it open on load */}
-            <summary>
-                <b id="largerfontb">What is solar and why is it good?</b>
-            </summary>
-            <img id="imagefloat" src={process.env.PUBLIC_URL+"benefitsofsolar.jpg"} alt="Benefits" width = "400" height = "400" floatleft/>
-            <img id="imagefloat2" src={process.env.PUBLIC_URL+"solarmap.png"} alt="Benefits" width = "300" height = "400" floatleft/>
+            <h1 id="largerfontb">Why is solar energy important?</h1>
+            <div id ="div"> {/* place "open" next to "details" to make it open on load */}
+            <img id="imagefloat" src={process.env.PUBLIC_URL+"solarmap.png"} alt="Benefits" width = "400" height = "500" floatleft/>
 
             <h3>The Enviornment</h3>
-            <p>
-                Solar energy is great for the enviornment. Some great benefits it provides are providing better air quality from
-                the reduction of carbon dioxide released into the air, which helps reduce the impacts of climate change.
-            </p>
+
+            <li id ="li">Reduces the amount of carbon dioxide released into the air</li>
+            <li id ="li">Provides better air quality</li>
+            <li id ="li">Helps mitigate climate change</li>
+
+
             <h3>Sunny Potential</h3>
-            <p>
-            Albania has a great potential for solar energy as it has between 2400-2500 hours of sunshine per year. The irradiation and PV electricity potential in Albania is promising, so expanding grid-based PV systems throughout Albania would increase electricity generation. The irradiation, or the amount of power received from the sun, and PV electricity potential in Albania is promising, so expanding grid-based PV systems throughout Albania would increase electricity generation. Depending on the municipality your business is located in, the irradiation can be high or low, so you can check out the graph to the right, which shows the irradiation more generally.
 
-            </p>
+            <li id ="li">Albania has extensive potential for solar energy with 2400-2500 hours of sunshine per year</li>
+            <li id ="li">The irradiation, or the amount of power received from the sun, is promising</li>
+            <li id ="li">The irradiation varies depending on the region your business is in, so you can check out the graph to the right, which shows the irradiation more generally</li>
+
   
-           </details>
+           </div>
             
             
             
-            <details close> {/* place "open" next to "details" to make it open on load */}
-            <summary>
-                <b id="largerfontb">Why is solar energy beneficial for businesses?</b>
-            </summary>
+            <div id ="div"> {/* place "open" next to "details" to make it open on load */}
+                <h1 id="largerfontb">What are the benefits of solar energy for your business?</h1>
 
-            <img id="imagefloat" src={process.env.PUBLIC_URL+"reasons.png"} alt="Reasons" width = "380" height = "700"/>
+            <img id="imagefloat" src={process.env.PUBLIC_URL+"reasons.png"} alt="Reasons" width = "430" height = "750"/>
            <h3>Reduced Energy Costs</h3>
-            <p>
-            Electricity bills can be problematic because they are expensive. Your electricity bill depends on how much energy is consumed and the rate your utility company charges. By using solar energy, your bill can be lowered by X%. You can look at switching to solar as replacing your electricity bill with monthly payments for your solar equipment. Electricity bills continue to get more expensive because utility companies face high costs to maintain and expand the grid. Solar power systems generally do not require much maintenance and most reliable solar panel manufacturers offer 20–25-year warranties.
-            </p>
-            <h3>Solar Tax Benefit</h3>
-            <p>
-            It has been discussed by the Ministry of Infrastructure and Energy of Albania in October 2022 to cancel the value-added tax on all machinery and other equipment imported to invest in solar energy.
-            </p>
-            <h3>More Energy Security and Independence</h3>
-            <p>
-            A business can achieve energy independence by adopting solar energy as they are becoming their own power providers. It provides electricity security to a business as electricity bills are seen as unpredictable. Costs of using solar are very predictable as many additionally pay service charges that go towards grid maintenance, charges for net metering, and other associated costs. If a business does not use 100% solar, then they will have some level of remaining electric bill that will vary through the year.
-            </p>
-            <h3>Green Business</h3>
-            <p>
-            Businesses can start going green by installing solar systems. This concept of going green is for business owners who invest their resources in finding the most efficient ways to sustain their companies for the better impact of the environment. This can attract a reduction in company costs by keeping general operating costs low over time, more eco-conscious employees to hire, and a better image and reputation of the company by leaving a lasting impression on your customers, potential customers, and vendors.
-            </p>
-            </details>
+           <li id ="li">Electricity bills can be problematic because they are expensive.</li>
+           <li id ="li">Your electricity bill depends on how much energy is consumed and the rate your utility company charges.</li>
+           <li id ="li">You can look at switching to solar as replacing your electricity bill with monthly payments for your solar equipment.</li>
+           <li id ="li">Electricity bills continue to get more expensive because utility companies face high costs to maintain and expand the grid.</li>
+           <li id ="li">You can avoid this increase of price by switching to solar!</li>
 
-
-            <details close> {/* place "open" next to "details" to make it open on load */}
-            <summary>
-                <b id="largerfontb">Should your business go solar?</b>
-            </summary>
             
-            <img id="imagefloat" src={process.env.PUBLIC_URL+"qs.png"} alt="Questions To Ask" width = "500" height = "500"/>
+            <h3>More Energy Security and Independence</h3>
+            <li id ="li">Becoming your own power providers</li>
+            <li id ="li">Costs of using solar are very predictable as many additionally pay service charges that go towards grid maintenance, charges for net metering, and other associated costs</li>
+            <li id ="li">Just remember if you do not go 100% solar, then you will have some level of remaining electric bill that will vary through the year.</li>
+            
+            <h3>Green Business</h3>
+            <li id ="li">You can go green by installing solar systems!</li>
+            <li id ="li">Going green means finding the most efficient ways to sustain your companies for the better impact of the environment</li>
+            <li id ="li">This attracts a reduction in company costs by keeping general operating costs low over time, more eco-conscious employees to hire, and a better image and reputation of the company by leaving a lasting impression on your customers, potential customers, and vendors.</li>
+
+            <h3>Solar Tax Benefit</h3>
+            <li id ="li">The value-added tax on all machinery and other equipment imported to invest in solar energy has been discussed to be cancelled!</li>
+            </div>
+
+
+            <div id ="div"> {/* place "open" next to "details" to make it open on load */}
+                <h1 id="largerfontb">Should your business go solar?</h1>
+            
+            <img id="imagefloat3" src={process.env.PUBLIC_URL+"qs.png"} alt="Questions To Ask" width = "500" height = "500"/>
             
             <h3>Why should I go solar?</h3>
             <p>
@@ -100,17 +100,15 @@ function Resources() {
             <p>
             Warranty depends on the manufacturer with most lasting between 20-25 years. Some solar companies also offer a few years of maintenance as well after they are installed on your roof.
             </p>
-            </details>
+            </div>
 
-            <details close> {/* place "open" next to "details" to make it open on load */}
-            <summary>
-                <b id="largerfontb">How can your business go solar?</b>
-            </summary>
+            <div id ="div"> {/* place "open" next to "details" to make it open on load */}
+                <h1 id="largerfontb">How can your business go solar?</h1>
             
             <img id="imagefloat" src={process.env.PUBLIC_URL+"how.png"} alt="How to Install" width = "400" height = "600"/>
 
             <h3>Solar Installers</h3>
-            <p>
+            <p id="p">
             Talk to different solar installation companies to determine if and how a solar system is right for you. Many companies offer technical and financial consulting, along with the full projections of a PV system on your roof. At these vendors will you be able to apply for a permit and start installation. Some solar companies and many solar manufacturers in which these installation companies get their panels from also offer periodic maintenance.
             </p>
 
@@ -138,7 +136,7 @@ function Resources() {
 
 
             <h3>Banks</h3>
-            <p>
+            <p id="p">
             Talk to different banks to determine if taking out a loan is right for your company. A couple banks, like Union Bank and ProCredit Bank, offer technical and financial assistance to help you determine if a solar system is right for you. These banks will often be partnered with solar installation companies they will get you in contact with.
             </p>
             <details close> {/* place "open" next to "details" to make it open on load */}
@@ -156,10 +154,10 @@ function Resources() {
             </details>
 
             <h3>Calculations</h3>
-            <p>
+            <p id="p">
             To determine which of the offers you are receiving from solar installation companies is the most suitable one for your business, use the app to do your own calculations! 
             </p>
-            </details>
+            </div>
 
             
             </div>
